314389881bcd439c9b5ae674f6c998c355aa2407
SatyaHarish9 <harishgvt@gmail.com>
2020-08-19 16:10:34 -0400

    Initial deployment of our empty pipeline

--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import { CfnOutput, Construct, Stage, StageProps } from '@aws-cdk/core';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as path from 'path';
 
 export class CdkpipelinesDemoStack extends cdk.Stack {
+  public readonly urlOutput: CfnOutput;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    
     // The code that defines your stack goes here
     // The Lambda function that contains the functionality
     const handler = new lambda.Function(this, 'Lambda', {
