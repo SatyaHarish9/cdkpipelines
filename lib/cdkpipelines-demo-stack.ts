0560e0dbbf3ec3fd735baeb47aa9e456ca0d8fd0
SatyaHarish9 <harishgvt@gmail.com>
2020-08-19 10:25:23 -0400

    cdk pipelines

--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -5,5 +5,21 @@ export class CdkpipelinesDemoStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // The Lambda function that contains the functionality
+    const handler = new lambda.Function(this, 'Lambda', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'handler.handler',
+      code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda')),
+    });
+
+    // An API Gateway to make the Lambda web-accessible
+    const gw = new apigw.LambdaRestApi(this, 'Gateway', {
+      description: 'Endpoint for a simple Lambda-powered web service',
+      handler,
+    });
+
+    this.urlOutput = new CfnOutput(this, 'Url', {
+      value: gw.url,
+    });
   }
 }
