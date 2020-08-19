a0b13d41b0b5131ed206f924d924267d1400ea61
SatyaHarish9 <harishgvt@gmail.com>
2020-08-19 18:18:33 -0400

    deploying to account 2

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
+
 
 const app = new App();
 
@@ -8,4 +10,9 @@ new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: '687214303997', region: 'us-east-2' },
 });
 
+new CdkpipelinesDemoStage(app, 'Dev', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
 app.synth();
+
